744b86ff6f7e7e0a2ff188eed89476c15592f4a8
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2026-05-15 17:12:21 +0100

    progress adding optuna to the pipeline

--- python_utils/utils.py
+++ python_utils/utils.py
@@ -6,6 +6,15 @@ import mlflow
 import pandas as pd
 import time
 import iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score, StratifiedGroupKFold
+import lightgbm as lgb
+
+SEED = 42  # TODO: Extract from AutomatedPipeline.cls
+NUM_CV_SPLITS = 10 # TODO: Extract from AutomatedPipeline.cls
+crossvalstrategy = StratifiedGroupKFold(n_splits=NUM_CV_SPLITS, shuffle=True, random_state=SEED)
+
 
 
 def measure_time_decorator(func):
@@ -154,3 +163,98 @@ def IRIS_DBQuery(schema: str, tablename: str, columns: str = "*", filters: str =
         print(f"IRIS_DBQuery Error: {str(e)}")
         iris._SYS.System.WriteToConsoleLog(f"IRIS_DBQuery Error: {str(e)}", 0, 2)
         return pd.DataFrame()
+
+
+
+
+class Objective:
+    def __init__(self, X, y, groups, params):
+        self.X = X
+        self.y = y
+        self.groups = groups
+        self.params = params
+
+    def __call__(self, trial):
+
+        parent_run_id = os.getenv("MLFLOW_PARENT_RUN_ID") # TODO: 
+
+        with mlflow.start_run(
+            run_name=f"trial_{trial.number}",
+            nested=True, parent_run_id=parent_run_id) as child_run:
+
+            pipeline = Pipeline([ # TODO: ADD AUGMENTATION STEPS HERE
+                ("scaler", StandardScaler()),
+                ("model", lgb.LGBMClassifier(**self.params))
+            ])
+
+            scores = cross_val_score(
+                pipeline,
+                self.X,
+                self.y,
+                cv=crossvalstrategy,
+                scoring="f1_macro",
+                groups=self.groups,  # Use datetime as the grouping variable to prevent data leakage
+                n_jobs=1,
+            )
+            
+            crossval_score = scores.mean()
+
+            # Log current trial's error metric
+            mlflow.log_metrics({"cv_f1_mean": crossval_score})
+            for fold_idx, score in enumerate(scores):
+                mlflow.log_metric(f"fold_{fold_idx}_f1", score)
+
+            # Make it easy to retrieve the best-performing child run later
+            trial.set_user_attr("run_id", child_run.info.run_id)
+
+            return crossval_score
+
+def objective(trial):
+    params = { # TODO: Define closer to AutomatedPipeline.cls and make sure to log them as parameters in MLflow for better tracking
+        "learning_rate": trial.suggest_float("learning_rate", 0.001, 0.2, log=True),  # CHANGEABLE
+        "max_depth": trial.suggest_int("max_depth", 3, 50),  # CHANGEABLE
+        "n_estimators": trial.suggest_int("n_estimators", 50, 1000),  # CHANGEABLE
+        "num_leaves": trial.suggest_categorical("num_leaves", [16, 31, 63, 127, 255]),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),  # CHANGEABLE
+        "max_bin": trial.suggest_categorical("max_bin", [63, 127, 255]),
+        "random_state": SEED,
+        "verbosity": -1,
+        "n_jobs": 1,
+    }
+
+    parent_run_id = os.getenv("MLFLOW_PARENT_RUN_ID")
+
+    with mlflow.start_run(
+        run_name=f"trial_{trial.number}",
+        nested=True,
+        parent_run_id=parent_run_id,
+        # tags={"mlflow.parentRunId": parent_run_id} if parent_run_id else None,
+    ) as child_run:
+
+
+        pipeline = Pipeline([ # TODO: ADD AUGMENTATION STEPS HERE
+            ("scaler", StandardScaler()),
+            ("model", lgb.LGBMClassifier(**params))
+        ])
+
+        scores = cross_val_score(
+            pipeline,
+            X,
+            y,
+            cv=crossvalstrategy,
+            scoring="f1_macro",
+            groups=groups,  # Use datetime as the grouping variable to prevent data leakage
+            n_jobs=1,
+        )
+        
+        crossval_score = scores.mean()
+
+        # Log current trial's error metric
+        mlflow.log_metrics({"cv_f1_mean": crossval_score})
+        for fold_idx, score in enumerate(scores):
+            mlflow.log_metric(f"fold_{fold_idx}_f1", score)
+
+        # Make it easy to retrieve the best-performing child run later
+        trial.set_user_attr("run_id", child_run.info.run_id)
+
+        return crossval_score
